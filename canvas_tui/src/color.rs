ce1e9e00cef32d6163f1df1446cf172a3c216abe
ThatEpicBanana <thatepicbanana132@gmail.com>
2023-08-07 16:41:31 -0400

    feat(colors): hex! and rgb

--- canvas_tui/src/color.rs
+++ canvas_tui/src/color.rs
@@ -23,6 +23,11 @@ impl Color {
         Self::new(val, val, val)
     }
 
+    #[must_use]
+    pub const fn from_array([r, g, b]: [u8; 3]) -> Self {
+        Self { r, g, b }
+    }
+
     pub fn paint<T: Display>(item: T, foreground: Option<Self>, background: Option<Self>) -> impl Display {
         let mut style = yansi::Paint::new(item);
         if let Some(foreground) = foreground { style = style.fg(foreground.into()); }
@@ -35,4 +40,34 @@ impl From<Color> for yansi::Color {
     fn from(value: Color) -> Self {
         Self::RGB(value.r, value.g, value.b)
     }
+}
+
+impl From<[u8; 3]> for Color {
+    fn from([r, g, b]: [u8; 3]) -> Self {
+        Self { r, g, b }
+    }
+}
+
+/// Creates a [`Color`] from a hex code string literal, see [`color_hex`] for the implementation
+///
+/// # Example
+///
+/// ```
+/// # use canvas_tui::prelude::*;
+/// assert_eq!(hex!("#ff00ff"), rgb(255, 0, 255));
+/// ```
+#[macro_export]
+macro_rules! hex {
+    ($lit:literal) => {
+        $crate::prelude::Color::from_array($crate::color_from_hex!($lit))
+    };
+}
+
+pub use crate::hex;
+
+#[allow(clippy::inline_always)] // it's essentially an alias (hopefully)
+#[inline(always)]
+#[must_use]
+pub const fn rgb(r: u8, g: u8, b: u8) -> Color {
+    Color::new(r, g, b)
 }
--- canvas_tui/src/prelude.rs
+++ canvas_tui/src/prelude.rs
@@ -3,7 +3,7 @@
 pub use crate::canvas::*;
 pub use crate::justification::*;
 pub use crate::Error;
-pub use crate::color::Color;
+pub use crate::color::{Color, hex, rgb};
 pub use crate::box_chars;
 pub use crate::result::*;
 pub use crate::num::Vec2;
